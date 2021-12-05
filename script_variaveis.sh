41ca9c612541eefecc4a8a60109f9121ab23627c
ISAIAS BARBOSA <64814667+ISAIAS-debug@users.noreply.github.com>
2021-12-05 11:08:19 -0300

    '

--- script_variaveis.sh
+++ script_variaveis.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+
+# Anotações feitas apartir do conteudo de shell script avancado
+
 VAR1="UMA STRING"
 VAR_COMANDO="mostrando a prioridade de swap: $(cat /proc/sys/vm/swappiness)"
 
@@ -14,7 +17,7 @@ echo $NOME_SCRIPT
 
 
 
-## trabalhando com condicionais
+# Trabalhando com condicionais
 
 
 var_swap=$(cat /proc/sys/vm/swappiness)
@@ -37,3 +40,38 @@ for i in $(seq 10); do
    ping -c2 192.168.115.$i
 
 done
+
+
+
+# Trabalhando com arquivos de configurações
+
+
+# var recebendo file de conf:
+ARQUIVO_CONF="configuraçao.cf"
+MENSAGEM="Mensagem de teste"
+CORES=
+MAISCULULA=
+
+# Validando se o arquivo existe:
+[ ! -r "$ARQUIVO_CONF" ]  && echo " Arquivo de configuração não existe "
+
+# criar a função parametro
+DefinirParametro (){
+ local parametro "$(echo $1 | cut -d =   -f 1)"
+ local valor="$(echo $1 | cut -d = -f 2)"
+ 
+ case "$parametro" in
+  USAR
+}
+
+while read -r linha
+do 
+[ "$(echo -e "$linha" | cut -c1" = "#" ) ] && continue     #se for vazio continuar, pula a linha
+[ ! "$linha" ]  && continue                                #pula linhas em branco
+DefinirParametro "$linha"
+# echo "$linha"
+done < ARQUIVO_CONF
+
+OBS: uso do cut para eliminar espaços e comentarios
+
+#parei aqui na aula.
